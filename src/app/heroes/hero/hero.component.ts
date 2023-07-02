d4f1f8a582ac34c968db97aaccca654d3e37debe
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-07-01 22:39:40 -0400

    1

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'hulk';
+  public age: number  = 50;
+
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
+  changeHero(): void {
+    this.name = 'Thor';
+  }
+
+  changeAge(): void {
+    this.age = 40;
+  }
+
+}
