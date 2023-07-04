b963a2d4dd302707fe1cea646499a191e26edeb7
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-07-04 11:45:34 -0400

    dbz component list

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DbzComponent } from './pages/main-page.components';
 import { CharacterListComponent } from './components/character-list/character-list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
     DbzComponent,
-    CharacterListComponent
+    CharacterListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
